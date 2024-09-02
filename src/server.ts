0ce996a8b1e59acbede739cff47fa9dc66d4c7c4
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-09-02 16:59:35 -0600

    [FEAT] Multiples cambios en el archivo de entrada server.ts
    - importando todo de dotenv
    - impoprtando la configuración de dotenv para hacer el uso de las varaibles de entorno de dotenv
    - establecemos la función listen para definir la ejecución del servidor
    - establecemos un log para que observemos la ejecución del servidor en un puerto en especificio

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,11 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
 const app = express()
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`This server is running in the port ${process.env.PORT}`);  
+})

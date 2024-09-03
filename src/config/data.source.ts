b1c16357b934d0a72d7469994c06874de2a1f5b2
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-09-02 19:04:31 -0600

    [FEAT] Exportando el AppDataSource

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const AppDataSource = new DataSource({
+export const AppDataSource = new DataSource({
     type: "mssql",
     host: process.env.HOST,
     port: process.env.PORTDB ? parseInt(process.env.PORTDB) : undefined,

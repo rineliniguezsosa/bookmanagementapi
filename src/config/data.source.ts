f9a15521ce8fbd79be8d72811b2a16c8884121f4
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-09-02 19:03:39 -0600

    [FEAT] Estableciendo los parametros de conecxión de la base de datos
    - importando todo de dotenv
    - importando las variables de entorno
    - estableciendo las variables de entorno en el datasource

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,1 +1,13 @@
 import { DataSource } from "typeorm"
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const AppDataSource = new DataSource({
+    type: "mssql",
+    host: process.env.HOST,
+    port: process.env.PORTDB ? parseInt(process.env.PORTDB) : undefined,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+})

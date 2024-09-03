2b2ddaf7c1fad59a408ea29c953bbed5621c7778
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-09-02 19:11:45 -0600

    [FEAT] Importamos el método connectdb en el archivo server.ts de la aplicación del servidor
    - llamamos al método connectdb

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { connectdb } from './config/connectdb';
 
 dotenv.config()
 
 const app = express()
-
+connectdb()
 
 app.listen(process.env.PORT,()=>{
     console.log(`This server is running in the port ${process.env.PORT}`);  

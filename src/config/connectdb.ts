83bea0badb5d7242a830f57588588d8475e33ef5
rineliniguezsosa <rineliniguezsosa@gmail.com>
2024-09-02 19:10:28 -0600

    [FEAT] Creando la función asincrona connectdb
    - esta función simplemente se encarga de conectrase a la base de datos

--- src/config/connectdb.ts
+++ src/config/connectdb.ts
@@ -1,1 +1,11 @@
 import { AppDataSource } from "./data.source";
+
+
+export const connectdb = async() =>{
+    try {
+        await AppDataSource.initialize();
+        console.log('connect to sql server database');
+    } catch (error) {
+        console.error('error to connect',error);
+    }
+}

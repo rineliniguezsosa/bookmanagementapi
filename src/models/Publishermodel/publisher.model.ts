0510d90639f9a680299f8da2cf024de7f1e13b4a
Rinel <rineliniguezsosa@gmail.com>
2024-09-04 16:27:23 -0600

    [FEAT] Pegando las columnas de la entidad Users en la entidad Publishers, que me doy cuenta que las columnas de la tabla Users van en Publishers estaban mal me equivoque en la definición de las columnas

--- src/models/Publishermodel/publisher.model.ts
+++ src/models/Publishermodel/publisher.model.ts
@@ -5,4 +5,13 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 export class PublisherEntity {
     @PrimaryGeneratedColumn()
     id!:number;
+
+    @Column({type:'varchar',length:50,nullable:true})
+    country!:string
+
+    @Column({type:'smallint',nullable:true})
+    established_year!:number
+
+    @Column({type:'timestamp'})
+    created_at!:Date
 }

29d73dfb6c76e08c791407e5b2e85fa8a7ae585c
Rinel <rineliniguezsosa@gmail.com>
2024-09-04 17:31:41 -0600

    [FEAT] Estableciendo la columna id en la tabla Reviews
    - estableciendo el decorador PrimaryGeneratedColumn para que la columna id sea llave primaria y se autogenere en automatico
    - columna id de tipo number

--- src/models/Reviesmodel/review.model.ts
+++ src/models/Reviesmodel/review.model.ts
@@ -2,5 +2,6 @@ import { Entity, PrimaryGeneratedColumn, Column,ManyToOne } from "typeorm"
 
 @Entity('Reviews')
 export class ReviewsEntity {
-    
+    @PrimaryGeneratedColumn()
+    id!:number;
 }

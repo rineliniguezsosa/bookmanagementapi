a899381c576e729c29e4811ceec24f28d1f3fe97
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 13:55:56 -0600

    [FEAT] Creando el archivo validateerrors.ts
    - desestructurando validationResult de express validator
    - desestructurando de express el Request, Response y NextFunction
    - creando la función asincrona validaterrors el cual se encargara de verificar los errores y devolver un status 400 para devover un error al cliente

---
+++ src/middleware/validateerrors.ts
@@ -0,0 +1,12 @@
+import { validationResult } from 'express-validator'
+import { Request,Response,NextFunction} from 'express'
+
+export const validaterrors = async(req:Request,resp:Response,next:NextFunction)=>{
+    const error = validationResult(req)
+
+    if(!error.isEmpty()){
+        return resp.status(400).json(error)
+    }
+
+    next()
+}

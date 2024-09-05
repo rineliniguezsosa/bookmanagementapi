5ba24df6b6bcef2b966fa9aae757bc5e6dafb5fb
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 15:01:07 -0600

    [FEAT] Estableciendo la fución check para validar de nueva cuenta el campo username
    - si username tiene una longitud maxima de 10 caracteres dolveremos el mensaje "'El campo debe de tener una longitud de 10 caracteres como minimo'"
    - estableciendo la función isLength con la opción minima de 10 caracteres

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -5,5 +5,6 @@ import { validaterrors } from '../middleware'
 export const uservalidation = [
     check('username').toLowerCase().trim().notEmpty().withMessage('El campo username no es vacio !!'),
     check('username').isAlpha().withMessage('El campo username solo acepta letras !!'),
+    check('username','El campo debe de tener una longitud de 10 caracteres como minimo').isLength({min:10}),
     check('email','El campo debe de estar en un formato valido de email').isEmail()
 ]

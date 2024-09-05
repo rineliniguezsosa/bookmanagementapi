af29d4d87e2cc699824ea1661fe058ec2c826f51
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 14:51:17 -0600

    [FEAT] Estableciendo la función check para verificar el campo email
    - la función check permite establecer un message espero que cuando envie un formato no valido de un email muestré el siguiente mensaje "El campo debe de estar en un formato valido de email", estamos probando es valido
    - establecemos la función isEmail donde establece un formato valdio de email para el campo email

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -4,5 +4,6 @@ import { validaterrors } from '../middleware'
 
 export const uservalidation = [
     check('username').toLowerCase().trim().notEmpty().withMessage('El campo username no es vacio !!'),
-    check('username').isAlpha().withMessage('El campo username solo acepta letras !!')
+    check('username').isAlpha().withMessage('El campo username solo acepta letras !!'),
+    check('email','El campo debe de estar en un formato valido de email').isEmail()
 ]

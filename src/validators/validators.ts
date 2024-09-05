f6fec5a091268294b64584682ffa425ab6007b2e
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 14:29:37 -0600

    [FEAT] Estableciendo la función withMessage en el campo username ya que si detecta que el campo username es vacio devolvera el mensaje "El campo username no es vacio !!"

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -3,5 +3,5 @@ import { validaterrors } from '../middleware'
 
 
 export const uservalidation = [
-    check('username').trim().notEmpty()
+    check('username').trim().notEmpty().withMessage('El campo username no es vacio !!')
 ]

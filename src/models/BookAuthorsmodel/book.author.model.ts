d4e1a322b39b531730722f6765bce2dffc15ebd3
Rinel <rineliniguezsosa@gmail.com>
2024-09-04 18:18:00 -0600

    [FEAT] Estableciendo la columna id en la tabla BookAuthors
    - la columna id es auto generada mediante el decorador PrimaryGeneratedColumn
    - la columna id es de tipo number

--- src/models/BookAuthorsmodel/book.author.model.ts
+++ src/models/BookAuthorsmodel/book.author.model.ts
@@ -2,5 +2,6 @@ import { Entity, PrimaryGeneratedColumn, Column,OneToMany } from "typeorm"
 
 @Entity('BookAuthors')
 export class BookAuthorsEntity{
-    
+    @PrimaryGeneratedColumn()
+    id!:number;
 }

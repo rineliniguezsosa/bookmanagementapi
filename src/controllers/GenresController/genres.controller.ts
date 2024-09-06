216f6392a2d2c30f97e35316b053019b2892075f
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 11:59:09 -0600

    [FEAT] Importando la entidad Genresentity en el archivo genres.controller.ts

--- src/controllers/GenresController/genres.controller.ts
+++ src/controllers/GenresController/genres.controller.ts
@@ -1,6 +1,6 @@
 import { Request,Response } from 'express';
 import { AppDataSource } from '../../config/data.source';
-
+import { GenresEntity } from '../../models';
 
 export const saveBookGenre = async(req:Request,resp:Response) =>{
     try {

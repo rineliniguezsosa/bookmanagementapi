145762466649802c68b940c46beed6cad7d61e02
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 12:17:50 -0600

    [FEAT] Estableciendo la respuesta del servidor para el caso de error donde devolvemos la respuesta 400 como un error del servidor y un mensaje para que el usuario sepa que ocurrio un error
    - establecemos un log para leer el error y que el usuario nunca sepa que sucedio

--- src/controllers/GenresController/genres.controller.ts
+++ src/controllers/GenresController/genres.controller.ts
@@ -13,6 +13,11 @@ export const saveBookGenre = async(req:Request,resp:Response) =>{
             menssage:data
         })
     } catch (error) {
+        console.log(error);
         
+        resp.status(400).json({
+            status:false,
+            message:'Porfavor vuelva a intentar, algo salio mal :('
+        })
     }
 }

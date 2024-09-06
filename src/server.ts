4c1ca5c939f4f7e82f289469b3383843b4912c53
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 20:45:16 -0600

    [FEAT] Cambios en el server.ts
    - importando la ruta bookgenresRoutes
    - estableciendo la ruta "/bookgenre" en nuestra aplicación y pasando la ruta bookgenresroutes para su lectura

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { connectdb } from './config/connectdb';
-import { userRoutes } from './routes';
+import { userRoutes,bookgenresRoutes } from './routes';
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use('/user',userRoutes)
+app.use('/bookgenre',bookgenresRoutes)
 connectdb()
 
 app.listen(process.env.PORT,()=>{

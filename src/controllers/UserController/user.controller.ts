8a09f0e7334efc678a2d32fdcead5a795a293ce0
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 20:00:32 -0600

    [FEAT] Multiples cambios en el controlador saveUser
    - importando bcrypt para encriptar los passwords
    - desestructurando la data del request.body
    - creando la variable salt para generar un hash
    - creando la variable newpassword para establecer el hash al atributo password y le establecemos el salt
    - creamos la variable data para almacenar el save de la data de username,email y el newpassword

--- src/controllers/UserController/user.controller.ts
+++ src/controllers/UserController/user.controller.ts
@@ -1,14 +1,19 @@
 import { Request,Response } from 'express';
 import { UserEntity } from '../../models';
 import { AppDataSource } from '../../config/data.source';
+import bcrypt from 'bcrypt';
 
 
 export const saveUser = async(req:Request,resp:Response) =>{
-    const data = req.body;
     try {
+        const { username,email,password } = req.body;
+    
+        const salt = bcrypt.genSaltSync(parseInt(`${process.env.SECRET}`))
+        const newpassword = bcrypt.hashSync(password,salt);
+
         const userRepository = AppDataSource.getRepository(UserEntity);
 
-        await userRepository.save(data)
+        const data = await userRepository.save({username,email,newpassword})
 
         resp.status(200).json({
             status:true,

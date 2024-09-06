bc7502d4be47f53765325ad1cc81d447676a09d5
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 19:26:16 -0600

    [FEAT] Multiples cambios en el método saveUser
    - importando la entidad UserEntity
    - importando el AppDataSource
    - creando la variable data para obtener el request body
    - creando la variable userRepository donde obtendremos del repositorio la entidad UserEntity
    - devolvemos la respuesta del servidor con un status 200 exitoso y con el formato del mensaje del servidor status: true y con el valor de la data en el menssage

--- src/controllers/UserController/user.controller.ts
+++ src/controllers/UserController/user.controller.ts
@@ -1,10 +1,21 @@
 import { Request,Response } from 'express';
+import { UserEntity } from '../../models';
+import { AppDataSource } from '../../config/data.source';
+
 
 export const saveUser = async(req:Request,resp:Response) =>{
-    console.log(req.body);
+    const data = req.body;
     try {
-        
+        const userRepository = AppDataSource.getRepository(UserEntity);
+
+        await userRepository.save(data)
+
+        resp.status(200).json({
+            status:true,
+            message:data
+        })
     } catch (error) {
+        console.log(error);
         
     }
 }

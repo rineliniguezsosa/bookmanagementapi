19d8584ac1326776fa26b2c28d5c50e8a0566224
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 19:07:20 -0600

    [FEAT] Cambiando el formato de las validaciones de los campos de entrada
    - establecemos un solo check de campo por validación ya que puede causar redundancia
    - cambie la prop min por max

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -3,12 +3,17 @@ import { validaterrors } from '../middleware'
 
 
 export const uservalidation = [
-    check('username').toLowerCase().trim().notEmpty().withMessage('El campo username no es vacio !!'),
-    check('username').isAlpha().withMessage('El campo username solo acepta letras !!'),
-    check('username','El campo debe de tener una longitud de 10 caracteres como minimo').isLength({min:10}),
-    check('email','El campo debe de estar en un formato valido de email').isEmail(),
-    check('email','El campo email no es vacio !!').notEmpty(),
-    check('password','El campo password debe de tener 10 caracteres como minimo').isLength({min:10}),
-    check('password','El campo password es requierido !!').notEmpty(),
+    check('username')
+        .trim()
+        .customSanitizer(value => value.toLowerCase())  // Transformar a minúsculas
+        .notEmpty().withMessage('El campo username no es vacio !!')
+        .isAlpha().withMessage('El campo username solo acepta letras !!')
+        .isLength({ max: 10 }).withMessage('El campo username debe de tener una longitud de 10 caracteres como minimo'),
+    check('email')
+        .notEmpty().withMessage('El campo email no es vacio !!')
+        .isEmail().withMessage('El campo debe de estar en un formato valido de email'),
+    check('password')
+        .notEmpty().withMessage('El campo password es requerido !!')
+        .isLength({ max: 10 }).withMessage('El campo password debe de tener una longitud de 10 caracteres como minimo'),
     validaterrors
 ]

dfe912fa9d8ac97df6d2ce32f824ad8549490064
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 19:17:50 -0600

    [FEAT} Cambiando los mensajes por campos requeridos en las validaciones de entrada cuando no es vacio

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -6,11 +6,11 @@ export const uservalidation = [
     check('username')
         .trim()
         .customSanitizer(value => value.toLowerCase())  // Transformar a minúsculas
-        .notEmpty().withMessage('El campo username no es vacio !!')
+        .notEmpty().withMessage('El campo username es requerido !!')
         .isAlpha().withMessage('El campo username solo acepta letras !!')
         .isLength({ max: 10 }).withMessage('El campo username debe de tener una longitud de 10 caracteres como minimo'),
     check('email')
-        .notEmpty().withMessage('El campo email no es vacio !!')
+        .notEmpty().withMessage('El campo email es requerido !!')
         .isEmail().withMessage('El campo debe de estar en un formato valido de email'),
     check('password')
         .notEmpty().withMessage('El campo password es requerido !!')

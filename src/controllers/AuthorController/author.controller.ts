7d744dcba2cce5c3b9acfcb2f7075784e3db7f8e
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 13:37:49 -0600

    [FEAT] Importando el AppDataSource y la entidad AuthorsEntity en el archivo author.controller.ts

--- src/controllers/AuthorController/author.controller.ts
+++ src/controllers/AuthorController/author.controller.ts
@@ -1,4 +1,6 @@
 import { Request,Response } from 'express'
+import { AppDataSource } from '../../config/data.source'
+import { AuthorsEntity } from '../../models'
 
 export const saveBookAuthor = async(req:Request,resp:Response) =>{
     try {

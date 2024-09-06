084d51938243b16cb28537b269e4df745af25fec
Rinel <rineliniguezsosa@gmail.com>
2024-09-05 20:17:17 -0600

    [FEAT] Exportando todo del archivo review.model

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from './Authormodel/author.model'
 export * from './Genresmodel/genres.model'
 export * from './Bookmodel/book.model'
 export * from './Usermodel/user.model'
+export * from './Reviesmodel/review.model';

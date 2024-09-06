1a81777b39b72e65e0c1799eb14436d7eb4e20db
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 11:58:16 -0600

    [FEAT] Importando el AppDataSource en el archivo genres.controller.ts

--- src/controllers/GenresController/genres.controller.ts
+++ src/controllers/GenresController/genres.controller.ts
@@ -1,4 +1,6 @@
 import { Request,Response } from 'express';
+import { AppDataSource } from '../../config/data.source';
+
 
 export const saveBookGenre = async(req:Request,resp:Response) =>{
     try {

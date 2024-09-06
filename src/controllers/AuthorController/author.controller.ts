e174d0a76b8cad7234b3c300588c366606c255a2
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 13:42:02 -0600

    [FEAT] Multiples cambios en el método saveBookAuthor
    - creando la variable authorRepository donde obtenemos el repositorio la entidad AuthorsEntity para poder establecer acciones en la entidad, insertar, obtener etc
    - creamos la variable data donde esperamos la inserción del rquest body
    - establecemos la respuesta del servidor con un status 200 donde devolvemos en formato json el status en true y el menssage con el valor de la inserción de la data

--- src/controllers/AuthorController/author.controller.ts
+++ src/controllers/AuthorController/author.controller.ts
@@ -4,7 +4,14 @@ import { AuthorsEntity } from '../../models'
 
 export const saveBookAuthor = async(req:Request,resp:Response) =>{
     try {
-        
+        const authorRepository = AppDataSource.getRepository(AuthorsEntity);
+
+        const data = await authorRepository.save(req.body);
+
+        resp.status(200).json({
+            status:true,
+            message:data
+        })
     } catch (error) {
         
     }

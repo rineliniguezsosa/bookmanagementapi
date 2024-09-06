217e7a6efdef52d39de55df2720b732cf432772a
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 13:49:37 -0600

    [FEAT] Cambios en DataSource de typeorm
    - importamos la entidad AuthorsEntity
    - establecemos la entidad AuthorsEntity en la opción entities para que sea leida la entidad

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,5 +1,5 @@
 import { DataSource } from "typeorm"
-import { UserEntity,ReviewsEntity,GenresEntity } from "../models";
+import { UserEntity,ReviewsEntity,GenresEntity,AuthorsEntity } from "../models";
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DB,
-    entities:[GenresEntity],
+    entities:[GenresEntity,AuthorsEntity],
     options: {
       encrypt: false,
       trustServerCertificate: true,

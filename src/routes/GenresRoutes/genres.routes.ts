3b929cfa87f57b275515f17fff936c1e3234e6a4
Rinel <rineliniguezsosa@gmail.com>
2024-09-07 13:41:02 -0600

    [FEAT] Cambiando la ruta "/by-id" por el param id en las rutas de los generos de los libros

--- src/routes/GenresRoutes/genres.routes.ts
+++ src/routes/GenresRoutes/genres.routes.ts
@@ -6,4 +6,4 @@ export const bookgenresRoutes = Router();
 
 bookgenresRoutes.post('/save',bookgenrevalidation,saveBookGenre)
 bookgenresRoutes.get('/all',getAllBookGenres)
-bookgenresRoutes.get('/by-id')
+bookgenresRoutes.get('/:id')

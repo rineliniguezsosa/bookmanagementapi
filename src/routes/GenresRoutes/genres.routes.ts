4ddbdd9836f9d4cc66ed726216046a211de77556
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 22:24:14 -0600

    [FEAT] Cambios en el archivo genres.routes.ts
    - importando el controllador getAllBookGenres
    - estableciendo el método getAllBookGenres como middleware en la ruta "/all"

--- src/routes/GenresRoutes/genres.routes.ts
+++ src/routes/GenresRoutes/genres.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { saveBookGenre } from '../../controllers';
+import { getAllBookGenres, saveBookGenre } from '../../controllers';
 import { bookgenrevalidation } from '../../validators';
 
 export const bookgenresRoutes = Router();
 
 bookgenresRoutes.post('/save',bookgenrevalidation,saveBookGenre)
-bookgenresRoutes.get('/all')
+bookgenresRoutes.get('/all',getAllBookGenres)

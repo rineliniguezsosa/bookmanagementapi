a512378da38d8e2f47b70c2ff6cbec4482367417
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 20:09:15 -0600

    [FEAT] Exportando todo el atrchivo publisher.model del archivo de barril index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,4 @@ export * from './Genresmodel/genres.model'
 export * from './Bookmodel/book.model'
 export * from './Usermodel/user.model'
 export * from './Reviesmodel/review.model';
+export * from './Publishermodel/publisher.model';

fd29004d837c8d84abb0c0ff9e62f8e1e3cace40
Rinel <rineliniguezsosa@gmail.com>
2024-09-07 14:29:05 -0600

    [FEAT] Cambios en el archivo genres.routes.ts
    - importando las validaciones genrebyidvalidation
    - estableciendo las validaciones genrebyidvalidation como middleware en la ruta del parametro "/:id"

--- src/routes/GenresRoutes/genres.routes.ts
+++ src/routes/GenresRoutes/genres.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { getAllBookGenres, saveBookGenre, getBookGenresById } from '../../controllers';
-import { bookgenrevalidation } from '../../validators';
+import { bookgenrevalidation,genrebyidvalidation } from '../../validators';
 
 export const bookgenresRoutes = Router();
 
 bookgenresRoutes.post('/save',bookgenrevalidation,saveBookGenre)
 bookgenresRoutes.get('/all',getAllBookGenres)
-bookgenresRoutes.get('/:id',getBookGenresById)
+bookgenresRoutes.get('/:id',genrebyidvalidation,getBookGenresById)

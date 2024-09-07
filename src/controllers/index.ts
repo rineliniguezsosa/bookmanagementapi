b31b165d6106ef529847adeb04acb32a6639cf6d
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 19:57:54 -0600

    [FEAT] Exportando todo del archivo publishers.controller en el archivo de barril index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from './UserController/user.controller';
 export * from './GenresController/genres.controller';
 export * from './AuthorController/author.controller';
+export * from './PublisherController/publishers.controller';

12c9cc669a89d2ebad0376092cf78a2383b82714
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 20:10:19 -0600

    [FEAT] Cambios en el archivo publishers.controller.ts
    - importando el AppDataSource
    - importando la entidad PublisherEntity

--- src/controllers/PublisherController/publishers.controller.ts
+++ src/controllers/PublisherController/publishers.controller.ts
@@ -1,4 +1,6 @@
 import { Request,Response } from 'express'
+import { AppDataSource } from '../../config/data.source'
+import { PublisherEntity } from '../../models'
 
 export const saveBookPublishers = async (req:Request,resp:Response) =>{
     try {

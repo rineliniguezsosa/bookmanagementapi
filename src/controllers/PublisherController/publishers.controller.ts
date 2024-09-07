d99bc5c938d27271f230a8562463871c14906e4c
Rinel <rineliniguezsosa@gmail.com>
2024-09-06 20:14:50 -0600

    [FEAT] Multiples cambios en el método saveBookPublishers
    - creando la variable publisherRepository donde obtenemos del repositorio la entidad PublisherEntity
    - creando la variable data donde almacemos la respuesta de la inserción de la entidad PublisherEntity
    - establecemos la respuesta del servidor con el status 200 y con el mensaje en formato json con el status en true y el message con la data de la respuesta de la inserción

--- src/controllers/PublisherController/publishers.controller.ts
+++ src/controllers/PublisherController/publishers.controller.ts
@@ -4,7 +4,14 @@ import { PublisherEntity } from '../../models'
 
 export const saveBookPublishers = async (req:Request,resp:Response) =>{
     try {
-        
+        const publisherRepository = AppDataSource.getRepository(PublisherEntity);
+
+        const data = await publisherRepository.save(req.body);
+
+        resp.status(200).json({
+            status:true,
+            message:data
+        })
     } catch (error) {
         
     }

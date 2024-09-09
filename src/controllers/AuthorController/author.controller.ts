d9770b956de40533c0c33b7958ad5797ad037b22
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 13:03:28 -0600

    [FEAT] Cambios en el archivo author.controller.ts, creando el método asincrono getBookAuthorById
    - creando el método getBookAuthorById
    - tipando los argumentos request y response
    - estableciendo el bloque try catch
    - desestructurando el parametro id
    - creando la variable idbookauthor para convertir el id en entero
    - creando la variable authorRepository donde vamos a leer del repositorio la entidad AuthorsEntity
    - creando la variable data donde esperamos la respuesta del método find
    - establecemos propiedades para filtrar el find donde usamos un where para buscar por id
    - establecemos el bloque if para validar el valor de la data y si es igual a cero entonces devolvemos el status del servidor como 400 y con el mensaje  "El id:${idbookauthor } no existe"
    - devolvemos un status 200 para devolver la respuesta del servidor con el status en true y con el message  de la data de la respuesta del s
    - para el caso de error devolvemos un log para leer el error
    - devolvemos el error del servidor con un status 400 y con el mensaje en formato json status false y message "Porfavor vuelva a intentar, algo salio mal :("

--- src/controllers/AuthorController/author.controller.ts
+++ src/controllers/AuthorController/author.controller.ts
@@ -42,3 +42,34 @@ export const getAllBookAuthors = async(req:Request,resp:Response) =>{
         })
     }
 }
+
+export const getBookAuthorById = async(req:Request,resp:Response) =>{
+    try {
+        const { id } = req.params
+        let idbookauthor = parseInt(id,10)
+
+        const authorRepository = AppDataSource.getRepository(AuthorsEntity);
+
+        const data = await authorRepository.find({ where: { id: idbookauthor   }})
+        
+        if(data.length === 0){
+          return resp.status(400).json({
+            status:false,
+            message:`El id:${idbookauthor } no existe`
+          })
+        }
+
+        return resp.status(200).json({
+            status:true,
+            message:data
+        })
+
+    } catch (error) {
+        console.log(error);
+        
+        return resp.status(400).json({
+            status:false,
+            message:'Porfavor vuelva a intentar, algo salio mal :('
+        })
+    }
+}

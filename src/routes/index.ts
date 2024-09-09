605a1d593dfecbf79724307330ee702b2e2dd141
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 12:07:46 -0600

    [FEAT] Exportando todo del archivo publishers.routes en el archivo de barril index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export * from './UserRoutes/user.routes'
 export * from './GenresRoutes/genres.routes'
 export * from './AuthorRoutes/author.routes'
+export * from './PublishersRoutes/publishers.routes';

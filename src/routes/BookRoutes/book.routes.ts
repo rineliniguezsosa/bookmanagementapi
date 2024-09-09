acde9409bd5dd551895195a0fddb16fb5c0c9b4b
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 17:25:17 -0600

    [FEAT] Cambios en el archivo book.routes.ts
    - importamos el método allBooks
    - creando la ruta "/all" de tipo get donde estableci el método allBooks como middleware

--- src/routes/BookRoutes/book.routes.ts
+++ src/routes/BookRoutes/book.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 import { bookvalidation } from '../../validators';
+import { allBooks } from '../../controllers';
 export const bookRoutes = Router();
 
 bookRoutes.post('/save',bookvalidation)
+bookRoutes.get('/all',allBooks)

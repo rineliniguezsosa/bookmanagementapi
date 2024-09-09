c57bdca95f59eddff899f4325196561bea7b2ff5
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 12:23:29 -0600

    [FEAT] Estableciendo retornos de respuesta de los mensajes del servidor en el método saveBookPublishers

--- src/controllers/PublisherController/publishers.controller.ts
+++ src/controllers/PublisherController/publishers.controller.ts
@@ -8,14 +8,14 @@ export const saveBookPublishers = async (req:Request,resp:Response) =>{
 
         const data = await publisherRepository.save(req.body);
 
-        resp.status(200).json({
+        return resp.status(200).json({
             status:true,
             message:data
         })
     } catch (error) {
         console.log(error);
         
-        resp.status(400).json({
+        return resp.status(400).json({
             status:false,
             message:'Porfavor vuelva a intentar, algo salio mal :('
         })

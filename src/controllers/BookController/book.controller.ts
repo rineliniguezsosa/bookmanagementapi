eca18bb81e29394e4c64c7b1ab4d22580a983485
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 17:31:15 -0600

    [FEAT] Multiples cambios en el archivo book.controller.ts
    - creando el método asincrono allBooks
    - tipando los argumentos request y response
    - creando la variable bookRepository donde vamos a leer la entidad BookEntity del repositorio
    - creamos la variable bookQuery donde establecemos un createQueryBuilder para crear una query y donde realice un inner join hacia las tablas Authors, Publishers y Genres
    - creamos la variable data donde esperamos la respuesta de obtener todos los registros del query bookQuery
    - estableciendo el retorno de la respuesta del servidor donde devolvemos un status 200 exitoso del servidor con el status en true y message con el valor de la data
    - establecemos un log para observar los errores
    - retornamos al usuario el error del servidor con el status en false y el message de "Porfavor vuelva a intentar, algo salio mal :("

--- src/controllers/BookController/book.controller.ts
+++ src/controllers/BookController/book.controller.ts
@@ -22,3 +22,29 @@ export const saveBook = async (req:Request,resp:Response) =>{
         })   
     }
 }
+
+export const allBooks = async (req:Request,resp:Response) =>{
+    try {
+        const bookRepository = AppDataSource.getRepository(BooksEntity);
+
+        const bookQuery = bookRepository.createQueryBuilder("Books")
+        .innerJoinAndSelect('Books.author_id','Authors')
+        .innerJoinAndSelect('Books.publisher_id','Publishers')
+        .innerJoinAndSelect('Books.genre_id','Genres')
+
+        const data = await bookQuery.getMany();
+
+        return resp.status(200).json({
+            status:true,
+            menssage:data
+        })
+
+    } catch (error) {
+        console.log(error);
+        
+        return resp.status(400).json({
+            status:false,
+            message:'Porfavor vuelva a intentar, algo salio mal :('
+        }) 
+    }
+}

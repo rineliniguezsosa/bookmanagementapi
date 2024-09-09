01612b52b294a805be4373383414a80709163a0f
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 13:07:51 -0600

    [FEAT] Cambios en el archivo author.routes.ts
    - importando el método getBookAuthorById
    - estableciendo la ruta get  "/:id" con el parametro id y pasando el método getBookAuthorById como middleware

--- src/routes/AuthorRoutes/author.routes.ts
+++ src/routes/AuthorRoutes/author.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { authorvalidation } from '../../validators';
-import { getAllBookAuthors } from '../../controllers';
+import { getAllBookAuthors, getBookAuthorById } from '../../controllers';
 export const authorRoutes = Router() 
 
 authorRoutes.post('/save',authorvalidation)
 authorRoutes.get("/all",getAllBookAuthors)
-authorRoutes.get('/:id')
+authorRoutes.get('/:id',getBookAuthorById)

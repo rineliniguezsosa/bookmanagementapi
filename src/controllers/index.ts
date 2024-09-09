68a22495ed636053b8a45e02eec4cd328b84c6e7
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 16:57:45 -0600

    [FEAT] Importando todo del archivo book.controller en el archivo de barril index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './UserController/user.controller';
 export * from './GenresController/genres.controller';
 export * from './AuthorController/author.controller';
 export * from './PublisherController/publishers.controller';
+export * from './BookController/book.controller';

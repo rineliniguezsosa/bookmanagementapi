36d3f0329bb754b172cc6c41ab1e2b2dcc6d8d8c
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 12:32:58 -0600

    [FEAT] Cambios en el archivo author.routes
    - importando el método  getAllBookAuthors
    - estableciendo la ruta "/all" para obtener todos los authores de los libros
    - la ruta "/all" sera de tipo get

--- src/routes/AuthorRoutes/author.routes.ts
+++ src/routes/AuthorRoutes/author.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { authorvalidation } from '../../validators';
+import { getAllBookAuthors } from '../../controllers';
 export const authorRoutes = Router() 
 
 authorRoutes.post('/save',authorvalidation)
+authorRoutes.get("/all",getAllBookAuthors)

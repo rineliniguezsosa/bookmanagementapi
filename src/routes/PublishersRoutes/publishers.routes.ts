e99cff6044b69e34d1c797a0fe2770787d93a3b6
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 12:14:39 -0600

    [FEAT] Cambios en el archivo de las rutas publishers.routes.ts
    - importando el método  getBookPublisherById
    - creando la ruta "/:id" con el param id, en esta ruta vamos a obtener a los editores por id

--- src/routes/PublishersRoutes/publishers.routes.ts
+++ src/routes/PublishersRoutes/publishers.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { publishersvalidation } from '../../validators';
-import { getAllBookPublishers, saveBookPublishers } from '../../controllers';
+import { getAllBookPublishers, getBookPublisherById, saveBookPublishers } from '../../controllers';
 export const publisherRoutes = Router()
 
 publisherRoutes.post('/save',publishersvalidation,saveBookPublishers)
 publisherRoutes.get('/all',getAllBookPublishers)
+publisherRoutes.get('/:id',getBookPublisherById)

e5173a959067d536e36fc516b54b73bdd7c8f1cf
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 17:55:35 -0600

    [FEAT] Cambios en el modelo Books
    - importando el decorador @JoinColumn
    - estableciendo el decorador @JoinColumn en las columnas author_id , publisher_id y genre_id para especificar las columnas de las llaves foraneas

--- src/models/Bookmodel/book.model.ts
+++ src/models/Bookmodel/book.model.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column,ManyToOne,OneToMany } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column,ManyToOne,OneToMany,JoinColumn } from "typeorm"
 import { AuthorsEntity } from "../Authormodel/author.model";
 import { PublisherEntity } from "../Publishermodel/publisher.model";
 import { GenresEntity } from "../Genresmodel/genres.model";
@@ -25,12 +25,15 @@ export class BooksEntity {
     created_at!:Date
 
     @ManyToOne(()=> AuthorsEntity, (author) => author.books, {nullable:false})
+    @JoinColumn()
     author_id!:AuthorsEntity;
 
     @ManyToOne(()=> PublisherEntity, (publisher) => publisher.books, {nullable:false})
+    @JoinColumn()
     publisher_id!:AuthorsEntity;
 
     @ManyToOne(()=> GenresEntity, (genres) => genres.books, {nullable:false})
+    @JoinColumn()
     genre_id!:GenresEntity;
 
     @OneToMany(() => ReviewsEntity, (review) => review.book_id)

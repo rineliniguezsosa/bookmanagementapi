c89fd71f6d4f7ff10a5b9cb622a9e1d5fd48d546
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 14:13:25 -0600

    [FEAT] Cambios en el archivo user.routes.ts
    - importando el método getUserById
    - estableciendo la ruta "/:id" de tipo get con el parametro id
    - pasamos el método getUserById como middleware de la ruta "/:id"

--- src/routes/UserRoutes/user.routes.ts
+++ src/routes/UserRoutes/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { saveUser, getAllUsers } from '../../controllers';
+import { saveUser, getAllUsers, getUserById } from '../../controllers';
 import { uservalidation } from '../../validators';
 
 export const userRoutes = Router()
 
 userRoutes.post('/save',uservalidation,saveUser)
 userRoutes.get('/all',getAllUsers)
+userRoutes.get('/:id',getUserById)

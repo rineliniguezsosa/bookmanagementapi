3ba2335cf4e5183dc2ac8720cfd6527624a162d7
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 14:02:00 -0600

    [FEAT] Cambios en la ruta user.routes
    - importamos el método getAllUsers
    - estableciendo la ruta "/all" de tipo get
    - establecemos el método getAllUsers en la ruta "/all" como middleware

--- src/routes/UserRoutes/user.routes.ts
+++ src/routes/UserRoutes/user.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { saveUser } from '../../controllers';
+import { saveUser, getAllUsers } from '../../controllers';
 import { uservalidation } from '../../validators';
 
 export const userRoutes = Router()
 
 userRoutes.post('/save',uservalidation,saveUser)
+userRoutes.get('/all',getAllUsers)

e7e3562454c15c1f020172a22f23c32d8bb9cac7
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 17:09:28 -0600

    [FEAT] Multiples cambios en el archivo book.controller.ts
    - creando el método asincrono saveBook
    - tipando el request y el response
    - estableciendo el bloque try catch
    - creando la variable bookRepository donde vamos a leer del repositio la entidad BooksEntity
    - creamos la variable data donde esperamos la respuesta del método save, pasamos el request.body en el método save
    - estableciendo el retorno de la respuesta con un status 200 exitoso en formato json con el status en true y el message con el valor de la data
    - estableciendo un log para observar los errores
    - estableciendo el retorno de la respuesta con un status 400 como error de servidor con el mensaje en formato en json con el status en false y message "Porfavor vuelva a intentar, algo salio mal :("

--- src/controllers/BookController/book.controller.ts
+++ src/controllers/BookController/book.controller.ts
@@ -1,3 +1,24 @@
 import { Request,Response } from 'express'
 import { AppDataSource } from '../../config/data.source'
 import { BooksEntity } from '../../models'
+
+
+export const saveBook = async (req:Request,resp:Response) =>{
+    try {
+        const bookRepository = AppDataSource.getRepository(BooksEntity);
+
+        const data = await bookRepository.save(req.body)
+
+        return resp.status(200).json({
+            status:true,
+            menssage:data
+        })
+    } catch (error) {
+        console.log(error);
+        
+        return resp.status(400).json({
+            status:false,
+            message:'Porfavor vuelva a intentar, algo salio mal :('
+        })   
+    }
+}

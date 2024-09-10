1abd379c8deb5b1b05e003690c4126ea01a84b96
Rinel <rineliniguezsosa@gmail.com>
2024-09-10 13:03:31 -0600

    [FEAT] Multiples cambios en el archivo review.controller.ts, creando el método asincrono saveBookReview
    - estableciendo el bloque try catch
    - creando la variable reviewsRepository donde vamos a leer del repositorio la entidad reviewEntity
    - creando la variable data donde esperamos la respuesta del método save para guardar información en la entidad Reviews
    - devolvemos la respuesta del servidor con un status 200 y con el formato de la respuesta en json, status en true y message con el valor de la data
    - establecemos un log para observar el error
    - establecemos la respuesta del servidor con un status 400 y con el formato del mensaje en json con status en false y el message "Porfavor vuelva a intentar, algo salio mal :("

--- src/controllers/ReviewController/review.controller.ts
+++ src/controllers/ReviewController/review.controller.ts
@@ -1,3 +1,23 @@
 import { Request,Response } from 'express'
 import { AppDataSource } from '../../config/data.source'
 import { ReviewsEntity } from '../../models'
+
+export const saveBookReview = async (req:Request,resp:Response) =>{
+    try {
+        const reviewsRepository = AppDataSource.getRepository(ReviewsEntity);
+
+        const data = await reviewsRepository.save(req.body)
+
+        return resp.status(200).json({
+            status:true,
+            menssage:data
+        })
+    } catch (error) {
+        console.log(error);
+        
+        return resp.status(400).json({
+            status:false,
+            message:'Porfavor vuelva a intentar, algo salio mal :('
+        })   
+    }
+}

a8215db19b2114da36fe2f1fe8b1fcd08709868a
Rinel <rineliniguezsosa@gmail.com>
2024-09-10 13:41:37 -0600

    [FEAT] Cambios en el archivo reviews.routes.ts
    - importando el método getBooksReviewById
    - creando la ruta "/:id" de tipo get con el param id y estableciendo el método getBooksReviewById en la ruta "/:id" como middleware

--- src/routes/ReviewsRoutes/reviews.routes.ts
+++ src/routes/ReviewsRoutes/reviews.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { reviewvalidation } from '../../validators';
-import { allBooksReviews, saveBookReview } from '../../controllers';
+import { allBooksReviews, getBooksReviewById, saveBookReview } from '../../controllers';
 
 export const reviewRoutes = Router();
 
 reviewRoutes.post('/save',reviewvalidation,saveBookReview)
 reviewRoutes.get('/all',allBooksReviews)
+reviewRoutes.get('/:id',getBooksReviewById)

e0787254ec3f9b532a3e37ee607fabf220eba1e9
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 20:34:27 -0600

    [FEAT] Multiples cambios en el archivo book.controller.ts, creando el método asincrono getBookById
    - estableciendo el bloque try catch
    - desestructurando el param id
    - creando la variable idbook donde convertimos a entero el id del param
    - creamos la variable bookRepository donde obtenemos del repositio la entidad BooksEntity para su lectura y manipulación
    - creamos la variable bookQuery donde creamos el inner join para obtener las relaciones de las tablas donde filtramos la busqueda del query mediante el where donde el is sea igual al idbook
    - creamos la variable data donde esperamos la respuesta de la obtención del todo el query
    - creamos el bloque if donde validamos el valor de la data y si es un array vacio devolvemos la respuesta del servidor con un mensaje en formato json con el stats en false y el menssage "El id:${idbook} no existe"
    - si todo sale bien devolvemos a respuesta del servidor con un status 200 y con el mensaje en formato json status en true y el message con el valor de la data
    - si la consulta sale mal devolvemos el valkor de la data y el mensaje del servidor en formato json status en false y message como "Porfavor vuelva a intentar, algo salio mal :("

--- src/controllers/BookController/book.controller.ts
+++ src/controllers/BookController/book.controller.ts
@@ -48,3 +48,40 @@ export const allBooks = async (req:Request,resp:Response) =>{
         }) 
     }
 }
+
+export const getBookById = async (req:Request,resp:Response) =>{
+    try {
+        const { id } = req.params;
+        let idbook = parseInt(id,10);
+
+        const bookRepository = AppDataSource.getRepository(BooksEntity);
+
+        const bookQuery = bookRepository.createQueryBuilder("Books")
+        .innerJoinAndSelect('Books.author','Authors')
+        .innerJoinAndSelect('Books.publisher','Publishers')
+        .innerJoinAndSelect('Books.genre','Genres')
+        .where({ id : idbook})
+
+        const data = await bookQuery.getMany();
+
+        if(data.length === 0){
+            return resp.status(400).json({
+                status:false,
+                message:`El id:${idbook} no existe`
+            })
+        }
+
+        return resp.status(200).json({
+            status:true,
+            menssage:data
+        })
+
+    } catch (error) {
+        console.log(error);
+        
+        return resp.status(400).json({
+            status:false,
+            message:'Porfavor vuelva a intentar, algo salio mal :('
+        }) 
+    }
+}

9f2c8501f8c6d971ee1f8b3f796c34a575b105b5
Rinel <rineliniguezsosa@gmail.com>
2024-09-09 20:21:31 -0600

    [FEAT] Cambios en el archivo book.routes.ts
    - importando el método getBookById
    - estableciendo el método getBookById como middleware en la ruta "/:id"

--- src/routes/BookRoutes/book.routes.ts
+++ src/routes/BookRoutes/book.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import { bookvalidation } from '../../validators';
-import { allBooks } from '../../controllers';
+import { allBooks, getBookById } from '../../controllers';
 export const bookRoutes = Router();
 
 bookRoutes.post('/save',bookvalidation)
 bookRoutes.get('/all',allBooks)
-bookRoutes.get('/:id')
+bookRoutes.get('/:id',getBookById)

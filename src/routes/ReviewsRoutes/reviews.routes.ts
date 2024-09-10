82a06c21eb533811a9af406c939164cb00502c63
Rinel <rineliniguezsosa@gmail.com>
2024-09-10 13:06:27 -0600

    [FEAT] Cambios en el archivo reviews.routes.ts
    - importando el método saveBookReview
    - estableciendo el método saveBookReview en la ruta "/save" como middleware

--- src/routes/ReviewsRoutes/reviews.routes.ts
+++ src/routes/ReviewsRoutes/reviews.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { reviewvalidation } from '../../validators';
+import { saveBookReview } from '../../controllers';
 
 export const reviewRoutes = Router();
 
-reviewRoutes.post('/save',reviewvalidation)
+reviewRoutes.post('/save',reviewvalidation,saveBookReview)

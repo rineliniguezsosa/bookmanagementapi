c509f2b33ecc956317600f5dd7bb9abf489b9df6
Rinel <rineliniguezsosa@gmail.com>
2024-09-17 16:46:05 -0600

    [FEAT] Estableciendo en la ruta "/save" de los autores la validacion de las entradas de authorvalidation

--- src/routes/AuthorRoutes/author.routes.ts
+++ src/routes/AuthorRoutes/author.routes.ts
@@ -3,6 +3,6 @@ import { authorvalidation } from '../../validators';
 import { getAllBookAuthors, getBookAuthorById } from '../../controllers';
 export const authorRoutes = Router() 
 
-authorRoutes.post('/save',authorvalidation)
+authorRoutes.post('/save',authorvalidation,authorvalidation)
 authorRoutes.get("/all",getAllBookAuthors)
 authorRoutes.get('/:id',getBookAuthorById)
